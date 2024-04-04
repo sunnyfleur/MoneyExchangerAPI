5cf0a9bb0fde8f504ca5eae0666870030b981b85
Tran Hoang Kiet <116096297+sunnyfleur@users.noreply.github.com>
2024-04-04 20:30:31 +0700

    Initial commit

---
+++ MoneyExchangerAPI/Controllers/MoneyExchangerAPI.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyExchangerAPI.Scripts;
+
+namespace MoneyExchangerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoneyExchangerAPIController : ControllerBase
+    {
+        private string exchangeRateAPI = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx";
+
+        [HttpGet]
+        public ActionResult<double> Get(string currencyID, double amount)
+        {
+            ExchangeRateGetter exchangeRateGetter = new ExchangeRateGetter(exchangeRateAPI);
+            var exchangeRateInfo = exchangeRateGetter.GetExchangeRate(currencyID); 
+
+            if (exchangeRateInfo != null)
+            {
+                double rate = Convert.ToDouble(exchangeRateInfo["Transfer"].Replace(",", ""));
+                double convertedAmount = amount * rate;
+                return convertedAmount;
+            }
+            else
+            {
+                return NotFound($"Không tìm thấy tỷ giá cho mã " + currencyID);
+            }
+        }
+    }
+}
---
+++ MoneyExchangerAPI/Scripts/ExchangeRateGetter.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml;
+
+namespace MoneyExchangerAPI.Scripts
+{
+    public class ExchangeRateGetter
+    {
+        private string exchangeRateAPI;
+
+        public ExchangeRateGetter(string exchangeRateAPI)
+        {
+            this.exchangeRateAPI = exchangeRateAPI;
+        }
+
+        public Dictionary<string, string> GetExchangeRate(string currencyCode)
+        {
+            var exchangeRateInfo = new Dictionary<string, string>();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(exchangeRateAPI); 
+
+            XmlNodeList exrateNodes = xmlDoc.SelectNodes("/ExrateList/Exrate");
+
+            foreach (XmlNode node in exrateNodes)
+            {
+                string code = node.Attributes["CurrencyCode"].Value;
+
+                if (code == currencyCode)
+                {
+                    exchangeRateInfo["Buy"] = node.Attributes["Buy"].Value;
+                    exchangeRateInfo["Transfer"] = node.Attributes["Transfer"].Value;
+                    exchangeRateInfo["Sell"] = node.Attributes["Sell"].Value;
+                    return exchangeRateInfo;
+                }
+            }
+
+            return null;
+        }
+    }
+}
